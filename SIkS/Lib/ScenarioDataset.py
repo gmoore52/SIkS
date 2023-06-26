cf45b571cdf2da4c67bc4687e23f1c4ec98caf27
gmoore52 <gavinmoore5225@gmail.com>
2023-06-26 14:45:18 -0500

    Mostly finished the ScenarioDataset API; needs more in-depth debugging and features but primary functions work

--- SIkS/Lib/ScenarioDataset.py
+++ SIkS/Lib/ScenarioDataset.py
@@ -7,7 +7,7 @@
 # to be used to store scenarios  #
 #                                #
 ##################################
-import ScenarioData as SData
+from . import ScenarioData as SData
 import json as js
 
 
@@ -16,51 +16,95 @@ import json as js
 # TODO: Define API that stores datasets of this form in JSON files and loads them
 x, y = 0, 0
 
-xi, yi = 0, 0
-
 HomogenousDataset = {
     "size":        [x, y],
-    "obstacles":   [SData.Shape()],
-    "roi":         [SData.Shape()],
+    "obstacles":   [SData.Shape],
+    "roi":         [SData.Shape],
     "sensor_data": SData.SensorData(),
-    "sensors":     [SData.Shape()],
-    "paths":       [SData.Path()]
+    "sensors":     [SData.Shape],
+    "paths":       [SData.Path]
 }
 
 HeterogenousDataset = {
-    "size":      [x, y],
-    "obstacles": [SData.Shape()],
-    "roi":       [SData.Shape()],
-    "sensors":   [SData.Sensor()],
-    "paths":     [SData.Path()]
+    "size":        [x, y],
+    "obstacles":   [SData.Shape],
+    "roi":         [SData.Shape],
+    "sensors":     [SData.Sensor],
+    "paths":       [SData.Path]
 }
 
 
 
 class ScenarioDataset():
-    def __init__(self, datasetMode="homo", filename="default.json") -> None:
+    def __init__(self, datasetMode="homo", filename="default.json", areaSize=(64, 64)):
         '''
         @ARGS
-        datasetMode => Can be either hetero or homo; not case sensitive
+        datasetMode => Can be either hetero or homo or load; not case sensitive
         filename => Defines the filename that the file will be stored under 
         '''
+        self.Dataset = {
+                        "size":        [areaSize[0], areaSize[1]],
+                        "obstacles":   [],
+                        "roi":         [],
+                        "paths":       []
+            }
+        if datasetMode.upper() == "LOAD":
+            self.LoadDataset()
+            self.DatasetType = "HOMO" if "sensor_data" in self.Dataset.keys() \
+                          else "HETERO"
+                          
+                          
+        
         self.DatasetType = datasetMode.upper()
+        self.FileName = filename
         
         assert self.DatasetType in ("HOMO", "HETERO"), \
            f"Invalid datasetMode parameter {datasetMode} set as {self.DatasetType}" 
+           
+        assert self.FileName[-5:] == ".json", \
+            f"Invalid filename, please use .json extension; Set filename is {filename}"
+            
+        assert len(areaSize) == 2 ,\
+            f"Invalid areaSize param; use a tuple/list of length 2 to contain each \
+            dimension; object passed: {areaSize}"
+           
+           
         
         
+        
+        if self.DatasetType == "HOMO":
+            self.Dataset["sensor_data"] = []
+            self.Dataset["sensors"]     = []
+        
+        if self.DatasetType == "HETERO":
+            self.Dataset["sensors"]     = []
+        
+        
+        self.StoreDataset()
             
         
-        pass
+        
     
     
-    def LoadDataset():
-        # LoadingDict = js.
+    def LoadDataset(self):
+        with open(self.FileName, 'r') as infile:
+            self.Dataset = js.load(infile)
+            assert all(key in self.Dataset.keys() \
+                       for key in ["size", "obstacles", "roi", "paths", "sensors"]) \
+                    and len(self.Dataset.keys()) in (5, 6), \
+                    f"Invalid Data loaded at file {self.FileName}; Data not in proper format"
+            self.DatasetType = "HOMO" if "sensor_data" in self.Dataset.keys() \
+                          else "HETERO"
+            infile.close()
         
         
-        pass
     
-    def StoreDataset():
+    def StoreDataset(self, p: list[SData.Shape]):
+        with open(self.FileName, 'w') as outfile:
+            js.dump(self.Dataset, outfile, indent=4)
+            outfile.close()
+            
+    def AddSensor(self, param: SData.Sensor):
         pass
+        
     
--- SIkS/SIkS.py
+++ SIkS/SIkS.py
@@ -7,3 +7,58 @@
 # will contain simulation code    #
 #                                 #
 ###################################
+import tkinter as tk
+from Lib.ScenarioDataset import ScenarioDataset as SDSet
+
+class DataManager():
+    def __init__(self, parent) -> None:
+        pass
+    
+    def saveData(self):
+        # Test function for testing the api manually
+        filename = "newname.json"
+        
+        newData = SDSet(filename=filename, datasetMode="hetero")
+        
+        newData.LoadDataset()
+        
+        
+        
+        print(newData.Dataset)
+    
+
+
+class MainFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        
+        
+        self.button = tk.Button(self, text='Save Dataset', height=2, width=8, highlightthickness=0, command=lambda: self.parent.Data.saveData())
+        self.button.grid(row=0, column=0, pady=(50,0), padx=(120,0))
+        
+        
+
+class SIkSInterface(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.parent.title("SIkS User Interface")
+        
+        self.Data = DataManager(self)
+        
+        self.Application = MainFrame(self)
+        self.Application.grid(row=0, column=0, sticky="NSEW")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    
+    root.geometry("400x400")
+    
+    window = SIkSInterface(root)
+    window.pack(fill="both", expand = True)
+    
+    # root.resizable()
+    root.mainloop()
